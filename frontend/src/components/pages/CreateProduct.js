e67f148791d97212248b775e8493425f72821e18
masopego <masopego@gmail.com>
2020-10-20 22:24:34 +0200

    Add fetch post and form

--- frontend/src/components/pages/CreateProduct.js
+++ frontend/src/components/pages/CreateProduct.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { postProduct } from "../../services/ProductService";
 
 function CreateProduct() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const resetForm = () => {
+    setName("");
+    setPrice("");
+  };
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    postProduct(name, price)
+      .then((resp) => {
+        alert("Producto añadido con éxito");
+        resetForm();
+      })
+      .catch((error) => {
+        alert("Ha habido un error. Revisa los datos introducidos");
+      });
+  };
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          placeholder="Plátano"
+          required
+          onChange={(ev) => setName(ev.target.value)}
+        />
+        <input
+          type="number"
+          name="price"
+          placeholder="3,75"
+          value={price}
+          required
+          onChange={(ev) => setPrice(parseFloat(ev.target.value))}
+        />
+        <input type="submit" />
+      </form>
       <p>Producto</p>
     </>
   );
--- frontend/src/services/ProductService.js
+++ frontend/src/services/ProductService.js
@@ -1,12 +1,17 @@
 const ENDPOINT = "http://localhost:8000/api/products";
 
 const getProducts = () => {
-  return fetch(ENDPOINT)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      return data;
-    });
+  return fetch(ENDPOINT).then((response) => response.json());
 };
 
-export { getProducts };
+const postProduct = (name, price) => {
+  return fetch(ENDPOINT, {
+    method: "POST",
+    body: JSON.stringify({ name, price }),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => response.json());
+};
+
+export { getProducts, postProduct };
